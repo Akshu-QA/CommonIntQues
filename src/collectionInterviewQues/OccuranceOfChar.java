732ed1ad9fa9b60fb300a12bdfa6635281cca7ce
ankit <ankitpawar193@gmail.com>
2025-06-17 11:53:18 +0530

    17 june ankit 1a

--- src/collectionInterviewQues/OccuranceOfChar.java
+++ src/collectionInterviewQues/OccuranceOfChar.java
@@ -1,10 +1,28 @@
 package collectionInterviewQues;
 
+import java.util.HashMap;
+
 public class OccuranceOfChar {
 
 	public static void main(String[] args) {
-		
 
+		String s = "ankit is highest paid QA";
+		char a[] = s.toCharArray();
+
+		HashMap<Character, Integer> hm = new HashMap();
+		for (char ref : a) {
+			if (hm.containsKey(ref)) {
+				int x = hm.get(ref);
+				hm.put(ref, x + 1);
+			} else {
+				hm.put(ref, 1);
+			}
+		}
+		// System.out.println(hm);
+
+		for (char r : hm.keySet()) {
+			System.out.println("repeated words " + r + " are : " + hm.get(r));
+		}
 	}
 
 }
