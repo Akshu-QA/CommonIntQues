4e33433f48a2897fdc90f386f804df5aa0de8286
ankit <ankitpawar193@gmail.com>
2025-06-21 21:58:12 +0530

    ankit commit 21 june 1c

---
+++ src/javaInterviewPrograms/FactorialNumber.java
@@ -0,0 +1,32 @@
+package javaInterviewPrograms;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	static int num;
+	static int fact = 1;
+
+	static int enterNum() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number u want factorial of ");
+		return num = sc.nextInt();
+	}
+
+	static void factorailLogic() {
+		for (int i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+	}
+
+	static void printFactorial() {
+		System.out.println(fact);
+	}
+
+	public static void main(String[] args) {
+		enterNum();
+		factorailLogic();
+		printFactorial();
+	}
+
+}
